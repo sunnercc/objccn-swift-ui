156e61b0d825530946bebbb47cefeff456fc06a7
陈晨晖 <chenhui.chen@yitu-inc.com>
2020-07-15 11:57:52 +0800

    init

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by 陈晨晖 on 2020/7/15.
+//  Copyright © 2020 sunner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, swiftUI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
