6bd433639db48a259334b43edacfcc268cb64f88
陈晨晖 <chenhui.chen@yitu-inc.com>
2020-07-15 16:47:58 +0800

    extract subviews

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,9 +8,61 @@
 
 import SwiftUI
 
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+            .font(.system(size: fontSize))
+            .foregroundColor(.white)
+            .frame(width: size.width, height: size.height)
+            .background(Color(backgroundColorName))
+            .cornerRadius(size.width / 2)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, swiftUI!")
+        HStack {
+            CalculatorButton(
+                title: "1",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground",
+                action: {
+                    print("button 1")
+                }
+            )
+            CalculatorButton(
+                title: "2",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground",
+                action: {
+                    print("button 2")
+                }
+            )
+            CalculatorButton(
+                title: "3",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "digitBackground",
+                action: {
+                    print("button 3")
+                }
+            )
+            CalculatorButton(
+                title: "+",
+                size: CGSize(width: 88, height: 88),
+                backgroundColorName: "operatorBackground",
+                action: {
+                    print("button +")
+                }
+            )
+        }
     }
 }
 
